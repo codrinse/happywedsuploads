aef0d44318a905b5d72ebce1177ca1cebb0e78af
Codrin Stefan Esanu <codrinstefan.esanu@cegeka.com>
2025-09-11 19:32:48 +0300

    Add app url

--- js/config.js
+++ js/config.js
@@ -1,5 +1,4 @@
 window.HW_CONFIG = {
-    WEB_APP_URL: 'https://script.google.com/macros/s/REPLACE_WITH_YOUR_EXEC_URL/exec',
+    WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbyHe7N_c-hBg9JXQaMWFdESuoLAOEc8kBgRpiyLp0Gnk9mnXZ3KL1tbwqggT4RMz8dH/exec',
     ADMIN_TOKEN: "Aah]!}C0*f;+x#2t=+T/"
   };
-  
