98530215df0d3920109dc5fa35fe8c14c1cdfb4f
Codrin Stefan Esanu <codrinstefan.esanu@cegeka.com>
2025-09-14 14:22:04 +0300

    Update web app url to new drive

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 window.HW_CONFIG = {
-    WEB_APP_URL: "https://script.google.com/macros/s/AKfycbxR6srYop8kR-JqRfcE-SRN3FbETrkJ4Kf0bR0TlO_tk1blQgPbQdV00CzMRMQ2Vrsp/exec",
+    WEB_APP_URL: "https://script.google.com/macros/s/AKfycbwnHcSv9wA40X5YGbkg1zVKjfZqLcDx1L8ainBRkFJTlp0xk8yFuq-SaW-XwLBMP4ErcA/exec",
     ADMIN_TOKEN: "Aah]!}C0*f;+x#2t=+T/"
   };
